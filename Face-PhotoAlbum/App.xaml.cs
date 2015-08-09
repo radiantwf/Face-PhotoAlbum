7c2cb1ccb3ce8dd4518dff79e696456ec1e7ec96
radiantwf <radiantwf@gmail.com>
2015-08-09 21:13:52 +0800

    Create

---
+++ Face-PhotoAlbum/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Face_PhotoAlbum
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+            Application.Current.ShutdownMode = System.Windows.ShutdownMode.OnExplicitShutdown;
+            Window top = new TopWindow();
+            bool? dialogResult = top.ShowDialog();
+            if ((dialogResult.HasValue == true) &&
+                (dialogResult.Value == true))
+            {
+                Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+            }
+            else
+            {
+                this.Shutdown();
+            }
+        }
+    }
+}
---
+++ Face-PhotoAlbum/TopWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Face_PhotoAlbum
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class TopWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public TopWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) this.DragMove();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += timer1_Tick;
+            timer.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
